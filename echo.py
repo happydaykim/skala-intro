3d21a5176d7ed19d0591d1cada066e603d4bc0ab
Gyeongrok Kim <hihappydaykim@daum.net>
2025-12-29 16:33:51 +0900

    에코 프로그램 개선

--- echo.py
+++ echo.py
@@ -1,5 +1,10 @@
-# 사용자로부터 문장을 입력받아 변수에 저장합니다.
-user_sentence = input("문장을 입력하세요: ")
+print("프로그램을 시작합니다. '!quit'을 입력하면 종료됩니다.")
 
-# 저장된 문장을 그대로 출력합니다.
-print(user_sentence)
+while True:
+    user_sentence = input("문장을 입력하세요: ")
+
+    if user_sentence == '!quit':
+        print("프로그램을 종료합니다.")
+        break
+
+    print(f"입력하신 문장: {user_sentence}")
